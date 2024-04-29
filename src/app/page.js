f2439445833a46f2a372e57a80b27a80defc38a3
alokverma749 <alokverma749@gmail.com>
2024-04-30 02:33:47 +0530

    fix: miniDrawer

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import About from "./about/page";
 import Signup from "./signup/page";
 import Login from "./login/page";
 import Event from "./event/page";
+import MiniDrawer from "@/components/Sidebar";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Signup />
       <Login/>
       <Event/>
+      <MiniDrawer/>
     </>
   );
 }
