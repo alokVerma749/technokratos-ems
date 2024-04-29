ada3102f327b9c0d93592d8e6739f755c8f0528e
Utkarsh Tiwari <uktiwari023@gmail.com>
2024-04-30 00:52:34 +0530

    create event card

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,9 +1,49 @@
-import React from 'react'
-
-const EventCard = () => {
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+ function EventCard() {
   return (
-    <div>EventCard</div>
-  )
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="100"
+          image="https://source.unsplash.com/random/100x100/?5"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Event Name
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <div className='flex items-center justify-between w-full p-2'>
+        <div className='flex gap-1'>
+          <PersonIcon/>
+          45
+        </div>
+        <div className='flex gap-1'>
+          <LocationOnIcon/>
+          Venue Name
+        </div>
+      </div>
+      <CardActions>
+        <button className="btn relative inline-flex items-center justify-start overflow-hidden font-medium transition-all bg-indigo-100 rounded hover:bg-white group py-1.5 px-2.5">
+        <span class="w-56 h-48 rounded bg-indigo-600 absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+        <span class="relative w-full text-left text-indigo-600 transition-colors duration-300 ease-in-out group-hover:text-white">Button Hover</span>
+      </button>
+      </CardActions>
+    </Card>
+  );
 }
 
 export default EventCard
