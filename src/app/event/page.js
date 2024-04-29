d64f885a5aaf70423e742ac470a176eb146314f3
alokverma749 <alokverma749@gmail.com>
2024-04-29 23:49:43 +0530

    initial commit

---
+++ src/app/event/page.js
@@ -0,0 +1,14 @@
+import EventCard from '@/components/EventCard'
+import React from 'react'
+
+const Event = () => {
+  return (
+    <div>
+      Event
+      <EventCard />
+    </div>
+
+  )
+}
+
+export default Event
