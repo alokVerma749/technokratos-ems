e8dff123536e46074da3566e6e77fb84a9951400
Utkarsh Tiwari <uktiwari023@gmail.com>
2024-04-30 11:50:20 +0530

    add image corousel

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,13 @@ import Login from "./login/page";
 import Event from "./event/page";
 import MiniDrawer from "@/components/Sidebar";
 import { useState } from "react";
+import { CarouselPlugin } from "@/components/ImageSlider";
 
 export default function Home() {
   const [isLogin, setIsLogin] = useState(true)
   return (
     <>
-      {isLogin ? <MiniDrawer /> : <Login />}
+      <CarouselPlugin/>
     </>
   );
 }
---
+++ src/components/ImageSlider/index.js
@@ -0,0 +1,54 @@
+import * as React from "react"
+import Autoplay from "embla-carousel-autoplay"
+ 
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from "next/image"
+ 
+export function CarouselPlugin() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  )
+ 
+  return (
+    <div className="flex w-full items-center justify-center">
+        <Carousel
+      plugins={[plugin.current]}
+      className="w-full max-w-5xl"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-video items-center justify-center p-2">
+                  <span className="text-4xl font-semibold">
+                    <Image
+                         height={4000}
+                         width={4000}
+                         src="https://source.unsplash.com/random/1600x900/?5"
+                         className=" hidden md:block"
+                         alt="random"
+                    />
+                  </span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+    </div>
+    
+  )
+}
