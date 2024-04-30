728d20c780be5a0f03395bf6b336ecde7f022434
sachinchauhan010 <010sssachin@gmail.com>
2024-04-30 15:36:16 +0530

    Member Added

--- src/app/about/member.js
+++ src/app/about/member.js
@@ -2,9 +2,9 @@ import React from "react";
 import Image from "next/image";
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import EmailIcon from '@mui/icons-material/Email';
-function Member() {
+function Member({id, name, contactNo, email}) {
   return (
-    <div className="flex flex-col gap-1 p-2 w-1/4 rounded-md border-2">
+    <div className="flex flex-col gap-1 border-2 my-6 shadow-xl rounded w-full md:w-[400px] lg:w-[370px] m-6 p-2 hover:shadow-2xl">
         <Image
           width="50"
           height="50"
@@ -14,12 +14,12 @@ function Member() {
         />
         <h2 className="w-full items-center flex text-xl px-2 font-bold text-indigo-600">Executive Member</h2>
         <div className="flex justify-between items-center w-full p-1">
-            <h2 className="text-black font-bold">John Doe</h2>
+            <h2 className="text-black font-bold">{name}</h2>
             <p className="text-gray-600 text-sm">MCA 1 <sup>st</sup> year</p>
         </div>
         <div className="flex items-center justify-between">
-            <div className="text-xs text-indigo-400"><LocalPhoneIcon/> 1234567890</div>
-            <div className="text-xs text-indigo-400"><EmailIcon/> xyzsi@somemail.com</div>
+            <div className="text-xs text-indigo-400"><LocalPhoneIcon/> {contactNo}</div>
+            <div className="text-xs text-indigo-400"><EmailIcon/>{email} </div>
         </div>
     </div>
   );
