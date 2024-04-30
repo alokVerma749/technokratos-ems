3dd7579100c9aebe865c089c353f5ff48579171b
sachinchauhan010 <010sssachin@gmail.com>
2024-04-30 11:20:31 +0530

    Event Page modified

--- src/app/event/page.js
+++ src/app/event/page.js
@@ -1,11 +1,50 @@
+'use client'
 import EventCard from '@/components/EventCard'
-import React from 'react'
+import { Description } from '@radix-ui/react-toast'
+import React, { useState } from 'react'
 
 const Event = () => {
+  const [countParticipants, setCountParticipants] =useState (0);
+  const events=[
+    {
+      id:1,
+      name:"Debate",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores provident voluptate, laudantium rem vel nemo ipsa reprehenderit itaque nisi accusantium, velit fugit architecto quis, sunt consequuntur molestias cum non modi?",
+      noOfMember:1,
+      venue:"Karmakar Hall", 
+    },
+    {
+      id:2,
+      name:"Extempore",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores provident voluptate, laudantium rem vel nemo ipsa reprehenderit itaque nisi accusantium, velit fugit architecto quis, sunt consequuntur molestias cum non modi?",
+      noOfMember:1,
+      venue:"Karmakar Hall", 
+    },
+    {
+      id:3,
+      name:"Creative Writing",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores provident voluptate, laudantium rem vel nemo ipsa reprehenderit itaque nisi accusantium, velit fugit architecto quis, sunt consequuntur molestias cum non modi?",
+      noOfMember:1,
+      venue:"Karmakar Hall", 
+    },
+    {
+      id:4,
+      name:"Hackthon",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores provident voluptate, laudantium rem vel nemo ipsa reprehenderit itaque nisi accusantium, velit fugit architecto quis, sunt consequuntur molestias cum non modi?",
+      noOfMember:3,
+      venue:"Karmakar Hall", 
+    }
+  ]
   return (
     <div>
+      <div className='flex justify-start items-center space-x-10 p-10'>
+        {
+          events.map((item)=>{
+            return <EventCard {...item} key={item.id}/>
+          })    
+        }
+      </div>
       Event
-      <EventCard />
     </div>
 
   )
--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,3 +1,4 @@
+'use client'
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -6,40 +7,39 @@ import Typography from '@mui/material/Typography';
 import {CardActionArea, CardActions } from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
- function EventCard() {
+function EventCard({ name, description, noOfMember, venue}) {
+ 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
+    <Card className='shadow-xl rounded w-1/3'>
+      <CardActionArea className=''>
         <CardMedia
           component="img"
-          height="100"
           image="https://source.unsplash.com/random/100x100/?5"
           alt="green iguana"
-        />
+         className='h-72 w-80'/>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Event Name
+          <Typography gutterBottom variant="h5" component="div" className='font-semibold'>
+            {name}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+          <Typography variant="body2" color="text.secondary" className=''>
+            {description}
           </Typography>
         </CardContent>
       </CardActionArea>
       <div className='flex items-center justify-between w-full p-2'>
         <div className='flex gap-1'>
           <PersonIcon/>
-          45
+          {noOfMember}
         </div>
         <div className='flex gap-1'>
           <LocationOnIcon/>
-          Venue Name
+          {venue}
         </div>
       </div>
       <CardActions>
         <button className="btn relative inline-flex items-center justify-start overflow-hidden font-medium transition-all bg-indigo-100 rounded hover:bg-white group py-1.5 px-2.5">
         <span class="w-56 h-48 rounded bg-indigo-600 absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-        <span class="relative w-full text-left text-indigo-600 transition-colors duration-300 ease-in-out group-hover:text-white">Button Hover</span>
+        <span class="relative w-full text-left text-indigo-600 transition-colors duration-300 ease-in-out group-hover:text-white">Register Now</span>
       </button>
       </CardActions>
     </Card>
