ec8e47253c4dd9b0679a2d0f847c2e0fafd7c206
alokverma749 <alokverma749@gmail.com>
2024-05-04 15:55:50 +0530

    fix: minor change

--- src/utils/checkAuthUser.js
+++ src/utils/checkAuthUser.js
@@ -3,7 +3,7 @@ import { cookies } from "next/headers"
 export const checkAuthUser = () => {
   const LoggedUser = cookies().get('user')
   return {
-    email: LoggedUser.value,
+    email: LoggedUser.value || '',
     isAdmin: LoggedUser.value === process.env.NEXT_PUBLIC_ADMIN_EMAIL
   }
 }
