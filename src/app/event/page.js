0c000316b4bb690d39c64150dc9ed251cafb4fd1
alokverma749 <alokverma749@gmail.com>
2024-05-04 17:58:27 +0530

    fix: try no-cache

--- src/app/event/page.js
+++ src/app/event/page.js
@@ -1,40 +1,39 @@
-"use client";
-import EventCard from "@/components/EventCard";
-import { Description } from "@radix-ui/react-toast";
-import React, { useEffect, useState } from "react";
+"use client"
+import EventCard from "@/components/EventCard"
+import React, { useEffect, useState } from "react"
 
 const Event = () => {
-  const [countParticipants, setCountParticipants] = useState(0);
+  const [countParticipants, setCountParticipants] = useState(0)
   const [events, setEvents] = useState([])
 
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await fetch('/api/getevents',{ next: { revalidate: 10 } })
+        const response = await fetch('/api/getevents', { cache: "no-cache" })
         if (!response.ok) {
-          throw new Error('Failed to fetch events');
+          throw new Error('Failed to fetch events')
         }
-        const data = await response.json();
-        setEvents(data.events);
+        const data = await response.json()
+        setEvents(data.events)
       } catch (error) {
-        console.error('Error fetching events:', error);
+        console.error('Error fetching events:', error)
       }
-    };
-  
-    fetchEvents();
-  }, []);
-  
+    }
+
+    fetchEvents()
+  }, [])
+
 
   return (
     <div>
       <p className="flex w-full items-center justify-center font-bold text-indigo-600 border-b-2 pb-2 uppercase text-3xl">Our Events</p>
       <div className="flex flex-col md:flex-row flex-wrap justify-start items-center space-x-10 p-0 md:p-10">
         {events.map((item) => {
-          return <EventCard {...item} key={item.id} />;
+          return <EventCard {...item} key={item.id} />
         })}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Event;
+export default Event
