e20ded623bc252c86e18f33986c8b15e6b4928cc
alokverma749 <alokverma749@gmail.com>
2024-05-06 18:16:59 +0530

    fix: SSR

--- src/app/event/page.js
+++ src/app/event/page.js
@@ -1,34 +1,26 @@
-"use client"
 import EventCard from "@/components/EventCard"
-import React, { useEffect, useState } from "react"
+import React from "react"
 
-const Event = () => {
-  const [countParticipants, setCountParticipants] = useState(0)
-  const [events, setEvents] = useState([])
-
-  useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const response = await fetch('/api/getevents', { cache: "no-store" })
-        if (!response.ok) {
-          throw new Error('Failed to fetch events')
-        }
-        const data = await response.json()
-        setEvents(data.events)
-      } catch (error) {
-        console.error('Error fetching events:', error)
-      }
+async function fetchEvents() {
+  try {
+    const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/api/getevents', { cache: "no-store" })
+    if (!response.ok) {
+      throw new Error('Failed to fetch events')
     }
-
-    fetchEvents()
-  }, [])
-
+    const data = await response.json()
+    return data.events
+  } catch (error) {
+    console.error('Error fetching events:', error)
+  }
+}
+const Event = async () => {
+  const events = await fetchEvents() || []
 
   return (
     <div>
       <p className="flex w-full items-center justify-center font-bold text-indigo-600 border-b-2 pb-2 uppercase text-3xl">Our Events</p>
       <div className="flex flex-col md:flex-row flex-wrap justify-start items-center space-x-10 p-0 md:p-10">
-        {events.map((item) => {
+        {events?.map((item) => {
           return <EventCard {...item} key={item.id} />
         })}
       </div>
