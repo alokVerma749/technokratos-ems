2032e5bd46d1e3fc2436a0fda67c2f542d62a9d1
Utkarsh Tiwari <uktiwari023@gmail.com>
2024-05-10 13:14:35 +0530

    add image of all members

--- src/app/about/member.js
+++ src/app/about/member.js
@@ -2,15 +2,15 @@ import React from "react";
 import Image from "next/image";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import EmailIcon from "@mui/icons-material/Email";
-function Member({ id, name, contactNo, email,std,branch }) {
+function Member({ id, name, contactNo, email,std,branch,imgsrc}) {
   return (
     <div className="flex flex-col gap-1 border-2 shadow-xl rounded w-full sm:w-[70%] sm:mx-auto md: md:w-[330px] lg:w-[360px] my-6 p-2 hover:shadow-2xl bg-gray-200">
       <Image
-        width="50"
-        height="50"
+        width="1000"
+        height="1000"
         alt=""
-        src="https://source.unsplash.com/1000x1000/?portrait"
-        className="object-cover w-full shadow p-2 pb-0 dark:bg-gray-500 h-[300px]"
+        src={imgsrc}
+        className="object-contain w-full shadow p-2 pb-0 dark:bg-gray-500 h-[300px]"
       />
       <h2 className="w-full items-center flex text-xl px-2 font-bold text-indigo-600">
         Executive Member
