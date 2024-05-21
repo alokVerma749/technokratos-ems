021ddc5e7fc20af6d67acca6d339a7e5623d412c
alokverma749 <alokverma749@gmail.com>
2024-05-21 23:37:32 +0530

    refactor: clean comments and format

--- src/app/api/members/route.js
+++ src/app/api/members/route.js
@@ -1,32 +1,31 @@
-import connectToDatabase from "../../../../config/mongodb";
-import Member from "@/models/member.model";
+import Member from "@/models/member.model"
+import connectToDatabase from "../../../../config/mongodb"
+
 export async function POST(request) {
   try {
-    const { name, course, branch, year, email } = await request.json();
+    const { name, course, branch, year, email } = await request.json()
 
-    await connectToDatabase();
+    await connectToDatabase()
 
-    const existingMember = await Member.findOne({ email }).exec();
+    const existingMember = await Member.findOne({ email }).exec()
     if (existingMember) {
-      return Response.json({ success: false, message: "user already exist" });
+      return Response.json({ success: false, message: "user already exist" })
     }
-    const memString=name+" "+course+" "+branch+ " "+year+"year";
-    console.log(memString, "&&&&&&&&&&")
-    const newEntry = new Member({ name, course, branch, year, email, memberString:memString });
+    const memString = name + " " + course + " " + branch + " " + year + "year"
+    const newEntry = new Member({ name, course, branch, year, email, memberString: memString })
 
     try {
-      await newEntry.save();
+      await newEntry.save()
 
       return Response.json({
         success: true,
         message: "Member registered successfully",
-      });
+      })
     } catch (error) {
-      console.log(saveError);
-      return Response.json({ success: false, message: "Error saving entry" });
+      console.log(saveError)
+      return Response.json({ success: false, message: "Error saving entry" })
     }
   } catch (error) {
-    return Response.json({ success: false, error: error.message });
+    return Response.json({ success: false, error: error.message })
   }
 }
-
